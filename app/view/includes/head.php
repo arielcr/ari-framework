9924746bab18d6d047be9411601456bbc91b7a8a
Ariel Orozco <ariel@pixelcr.com>
2012-10-08 11:13:05 -0600

    - modificacion a head.php

--- app/view/includes/head.php
+++ app/view/includes/head.php
@@ -1,9 +1,15 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $titulo; ?></title>
 <link href="<?php echo SITE_URL; ?>css/style.css" rel="stylesheet" type="text/css" />
-<link type="text/css" href="<?php echo SITE_URL; ?>css/ui-lightness/jquery-ui-1.8.24.custom.css" rel="stylesheet" />
+<link href="<?php echo SITE_URL; ?>css/nicetables.css" rel="stylesheet" type="text/css" />
+
+<link type="text/css" href="<?php echo SITE_URL; ?>css/demo_table_jui.css" rel="stylesheet" />
+<link type="text/css" href="<?php echo SITE_URL; ?>css/smoothness/jquery-ui-1.8.4.custom.css" rel="stylesheet" />
+
 <script type="text/javascript" src="<?php echo SITE_URL; ?>js/jquery-1.8.2.min.js"></script>
 <script type="text/javascript" src="<?php echo SITE_URL; ?>js/jquery-ui-1.8.24.custom.min.js"></script>
 <script type="text/javascript" src="<?php echo SITE_URL; ?>js/jquery.validate.js"></script>
+<script type="text/javascript" src="<?php echo SITE_URL; ?>js/messages_es.js"></script>
+<script type="text/javascript" src="<?php echo SITE_URL; ?>js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="<?php echo SITE_URL; ?>js/functions.js"></script>
 
