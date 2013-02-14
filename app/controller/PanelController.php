3e9d0df9c968cb1ecbccb92af52c744419457de0
andres <andres@andres>
2013-02-14 15:02:10 -0600

    Se implementa la clase para determinar si el usuario está utilizando un dispositivo movil

--- app/controller/PanelController.php
+++ app/controller/PanelController.php
@@ -4,15 +4,20 @@ class PanelController extends Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->userDevice = new IsMobile();
     }
 
     public function index() {
         if (!isset($this->session->usuario)) {
             $this->response->redirect("login");
         } else {
-            $view = new View('panel');
-            $view->titulo = 'Panel';
+            $view          = new View('panel');
+            $view->titulo  = 'Panel';
             $view->usuario = $this->session->usuario;
+
+            if ($this->userDevice->isMobile()) {
+                echo 'Es un dispositivo movil';
+            }
 
             echo $view->display();
         }
