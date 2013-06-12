ebc4ebf01dc2b6eadfdd6f8193545040a1ccbb71
andres <andres@andres>
2013-06-12 09:00:48 -0600

    Modificacion del dispatcher para soportar varios niveles de carpetas. Actualmente esta seteado para trabajar con tres niveles de directorios, hay que modificar para que pueda trabajar con cualquier cantidad de niveles. Contador para los campos del array que asigna el controller/method/arguments

--- lib/common/Dispatcher.php
+++ lib/common/Dispatcher.php
@@ -1,26 +1,65 @@
 <?php
 
+/* class Dispatcher {
+
+  public static function dispatch() {
+
+  //REMOTO
+  //$url = explode('/', $_SERVER['REQUEST_URI']);
+  $url = explode('/', trim($_SERVER['REQUEST_URI'], "/"));
+
+  array_shift($url);
+
+  $controller = !empty($url[0]) ? ucfirst($url[0]) . 'Controller' : 'DefaultController';
+
+  $method = !empty($url[1]) ? $url[1] : 'index';
+
+  $arg = !empty($url[2]) ? $url[2] : NULL;
+
+  $cont = new $controller;
+
+  $cont->$method($arg);
+  }
+
+  } */
+
 class Dispatcher {
 
+    /**
+     * Dispatch function - delegates action to other function and passes parameters to it
+     */
     public static function dispatch() {
 
-        //REMOTO
-        //$url = explode('/', $_SERVER['REQUEST_URI']);
-        $url = explode('/', trim($_SERVER['REQUEST_URI'], "/"));
+        // Stear clear, allow only leters, numbers and slashes
+        if (!empty($raw_route) and preg_match('/^[\p{L}\/\d]++$/uD', $_SERVER["REQUEST_URI"]) == 0) {
+            die("Invalid URL");
+        }
 
-        array_shift($url);
+        // Extract parameters
+        $url_pieces = explode("/", $_SERVER["REQUEST_URI"]);
+        array_shift($url_pieces);
 
-        $controller = !empty($url[0]) ? ucfirst($url[0]) . 'Controller' : 'DefaultController';
+        $site_root_pieces = explode("/", DIR_ROOT);
 
-        $method = !empty($url[1]) ? $url[1] : 'index';
+        $diff = array_diff($url_pieces, $site_root_pieces);
 
-        $arg = !empty($url[2]) ? $url[2] : NULL;
+        $controller = !empty($diff[3]) ? ucfirst($diff[3]) . 'Controller' : 'DefaultController';
 
+        $method = array();
+
+        if (count($url_pieces) > 3) {
+            if (!empty($url_pieces[4])) {
+                $method = $url_pieces[4];
+            } else {
+                $method = 'index';
+            }
+        }
+
+        $arg = !empty($url[5]) ? $url[5] : NULL;
+
+        // All checked, execute requested funcion with provided parameters
         $cont = new $controller;
-
         $cont->$method($arg);
     }
 
 }
-
-?>
